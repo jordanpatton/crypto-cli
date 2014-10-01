eea9577e12e0077ff017a07fbe56b85d8f10468e
Jordan Patton <jordanpatton@gmail.com>
2014-10-01 14:47:21 -0400

    Renamed "common" to "lib".

--- encrypt.js
+++ encrypt.js
@@ -1,7 +1,7 @@
-var common = require('./lib');
+var lib = require('./lib');
 
 
 var args = process.argv.slice(2);
 for(var i=0; i<args.length; i++) {
-  console.log(args[i], common.encrypt(args[i]));
+  console.log(args[i], lib.encrypt(args[i]));
 }
